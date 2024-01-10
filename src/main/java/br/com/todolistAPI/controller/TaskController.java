2ab274cf26cc3c4d532e80656b6c5b5b52906701
Davi Pedro da Silva <davisilvaps02@gmail.com>
2024-01-10 15:51:42 -0300

    CRUD endpoints and validations

--- src/main/java/br/com/todolistAPI/controller/TaskController.java
+++ src/main/java/br/com/todolistAPI/controller/TaskController.java
@@ -2,34 +2,94 @@ package br.com.todolistAPI.controller;
 
 import br.com.todolistAPI.service.TaskService;
 import br.com.todolistAPI.task.Task;
+import br.com.todolistAPI.task.TaskDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/task")
 public class TaskController {
-
+    @Autowired
     TaskService taskService = new TaskService();
 
-    @GetMapping("/all")
+    @GetMapping("/")
     public ResponseEntity<List<Task>> getAllTasks(){
         List<Task> tasks = taskService.getAllTasks();
+        if(tasks.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(tasks);
     }
 
     @GetMapping("/{title}")
-    public Task getByTitle(@PathVariable String title){
-        Optional<Task> task = taskService.getByTitle(title);
-        return task.orElseThrow();
+    public ResponseEntity<Task> getByTitle(@PathVariable String title){
+        Optional<Task> taskByTitle = taskService.getByTitle(title);
+        Task task = taskByTitle.orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Task not found"));
+        return ResponseEntity.ok().body(task);
     }
 
     @GetMapping("/{creationDate}")
-    public Task getByCreationDate(@PathVariable LocalDate creationDate){
-        Optional<Task> task = taskService.getByCreationDate(creationDate);
-        return task.orElseThrow();
+    public ResponseEntity<Task> getByCreationDate(@PathVariable LocalDate creationDate){
+        Optional<Task> taskByCreationDate = taskService.getByCreationDate(creationDate);
+        Task task = taskByCreationDate.orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Task not found"));
+        return ResponseEntity.ok().body(task);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTask(@PathVariable UUID id){
+        try {
+            taskService.deleteTask(id);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        return ResponseEntity.ok("Deleted successfully");
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Object> createTask(@RequestBody @Valid TaskDTO taskDTO, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT, "The task could not be created \n Validation error:" + errors);
+        }
+        taskService.createTask(taskDTO);
+        return ResponseEntity.status(201).build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<String> patchTask(@RequestBody @Valid TaskDTO taskDTO, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "The task could not be updated \n Validation error:" + errors);
+        }
+        taskService.patchTask(taskDTO);
+        return ResponseEntity.ok("Updated successfully");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> putTask(@RequestBody @Valid TaskDTO taskDTO, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "The task could not be updated \n Validation error:" + errors);
+        }
+        taskService.putTask(taskDTO);
+        return ResponseEntity.ok("Updated successfully");
+    }
+
+
 }
--- src/main/java/br/com/todolistAPI/task/TaskDTO.java
+++ src/main/java/br/com/todolistAPI/task/TaskDTO.java
@@ -1,12 +1,57 @@
 package br.com.todolistAPI.task;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record TaskDTO(String title,
-                      String description,
-                      @JsonAlias("conclusion_date") LocalDate conclusionDate){
+public class TaskDTO{
+    @JsonIgnore private UUID id;
+    @NotBlank private String title;
+    private String description;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonAlias("conclusion_date")
+    private LocalDate conclusionDate;
+    @JsonIgnore private LocalDate lastUpdate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getConclusionDate() {
+        return conclusionDate;
+    }
+
+    public void setConclusionDate(LocalDate conclusionDate) {
+        this.conclusionDate = conclusionDate;
+    }
+
+    public LocalDate getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDate lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 }
