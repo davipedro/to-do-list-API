18ea51de0ae0476b812efb5f76fe96aae72b6ad2
Davi Pedro da Silva <davisilvaps02@gmail.com>
2024-01-11 09:54:10 -0300

    Switching to Record

--- src/main/java/br/com/todolistAPI/task/TaskDTO.java
+++ src/main/java/br/com/todolistAPI/task/TaskDTO.java
@@ -1,57 +1,14 @@
 package br.com.todolistAPI.task;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.*;
 import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TaskDTO{
-    @JsonIgnore private UUID id;
-    @NotBlank private String title;
-    private String description;
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @JsonAlias("conclusion_date")
-    private LocalDate conclusionDate;
-    @JsonIgnore private LocalDate lastUpdate;
+public record TaskDTO (@JsonAlias("id") Long id,
+                       @JsonAlias("title") @NotBlank String title,
+                       @JsonAlias("description") @NotBlank String description,
+                       @JsonAlias("conclusion_date") LocalDate conclusionDate){
 
-    public UUID getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getConclusionDate() {
-        return conclusionDate;
-    }
-
-    public void setConclusionDate(LocalDate conclusionDate) {
-        this.conclusionDate = conclusionDate;
-    }
-
-    public LocalDate getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(LocalDate lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
 }
