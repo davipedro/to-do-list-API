9c9834444e16ccda7cee047a2c2aba0604131315
Davi Pedro da Silva <davisilvaps02@gmail.com>
2024-01-11 09:54:23 -0300

    Some adjustments

--- src/main/java/br/com/todolistAPI/task/TaskRepository.java
+++ src/main/java/br/com/todolistAPI/task/TaskRepository.java
@@ -3,11 +3,11 @@ package br.com.todolistAPI.task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-        Optional<Task> findByTitleContainingIgnoreCase(String title);
+        Optional<List<Task>> findByTitleContainingIgnoreCase(String title);
         Optional<Task> findByCreationDate(LocalDate creationDate);
-        Optional<Task> findByTaskId(UUID taskId);
 }
