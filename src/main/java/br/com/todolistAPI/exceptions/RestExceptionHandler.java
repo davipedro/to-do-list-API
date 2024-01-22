a30d6a64237d5671deac3eb6d77aa5433bb78063
Davi Pedro <davisilvaps02@gmail.com>
2024-01-22 13:25:57 -0300

    add duplicatedTaskHandler

--- src/main/java/br/com/todolistAPI/exceptions/RestExceptionHandler.java
+++ src/main/java/br/com/todolistAPI/exceptions/RestExceptionHandler.java
@@ -7,10 +7,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.sql.SQLException;
+
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(TaskNotFoundException.class)
     private ResponseEntity<String> taskNotFoundHandler(){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not found");
+    }
+    @ExceptionHandler(SQLException.class)
+    private ResponseEntity<String> duplicatedTaskHandler(){
+        return ResponseEntity.badRequest().body("A task with this name already exist");
     }
 }
