41e65c22aab4e8d67ca7901dddf945fef947416d
Davi Pedro <davisilvaps02@gmail.com>
2024-01-25 14:22:27 -0300

    User exceptions handlers

--- src/main/java/br/com/todolistAPI/exceptions/RestExceptionHandler.java
+++ src/main/java/br/com/todolistAPI/exceptions/RestExceptionHandler.java
@@ -1,6 +1,9 @@
 package br.com.todolistAPI.exceptions;
 
 import br.com.todolistAPI.exceptions.task.TaskNotFoundException;
+import br.com.todolistAPI.exceptions.user.AlreadyRegisteredUserException;
+import br.com.todolistAPI.exceptions.user.RoleNotFoundException;
+import br.com.todolistAPI.exceptions.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,5 +21,17 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(SQLException.class)
     private ResponseEntity<String> duplicatedTaskHandler(){
         return ResponseEntity.badRequest().body("A task with this name already exist");
+    }
+    @ExceptionHandler(AlreadyRegisteredUserException.class)
+    private ResponseEntity<String> RegisteredUserHandler(){
+        return ResponseEntity.badRequest().body("User already registered");
+    }
+    @ExceptionHandler(RoleNotFoundException.class)
+    private ResponseEntity<String> RoleNotFoundHandler(){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Role not found");
+    }
+    @ExceptionHandler(UserNotFoundException.class)
+    private ResponseEntity<String> UserNotFoundHandler(){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
     }
 }
