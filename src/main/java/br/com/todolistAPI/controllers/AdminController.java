ec6370f934242eb1973d3e38e379fc5c6ff00510
Davi Pedro <davisilvaps02@gmail.com>
2024-01-27 19:15:03 -0300

    Swagger documentation and deleteAdminById

--- src/main/java/br/com/todolistAPI/controllers/AdminController.java
+++ src/main/java/br/com/todolistAPI/controllers/AdminController.java
@@ -1,14 +1,13 @@
 package br.com.todolistAPI.controllers;
 
+import br.com.todolistAPI.DTOs.AuthenticationDTO;
 import br.com.todolistAPI.DTOs.LoginResponseDTO;
 import br.com.todolistAPI.DTOs.UserResponseDTO;
 import br.com.todolistAPI.config.security.TokenService;
-import br.com.todolistAPI.DTOs.AuthenticationDTO;
-import br.com.todolistAPI.DTOs.RegisterDTO;
 import br.com.todolistAPI.domain.user.User;
-import br.com.todolistAPI.domain.user.UserRole;
 import br.com.todolistAPI.services.AdminService;
-import br.com.todolistAPI.services.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,21 +19,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/admin")
+@Tag(name = "Admin Controller", description = "End points para cadastro e login de usuários com permissões de administrador")
 public class AdminController {
 
     private final AuthenticationManager authenticationManager;
-    private final AuthenticationService authenticationService;
     private final AdminService adminService;
 
     @Autowired
     TokenService tokenService;
 
-    public AdminController(AuthenticationManager authenticationManager, AuthenticationService authenticationService, AdminService adminService) {
+    public AdminController(AuthenticationManager authenticationManager, AdminService adminService) {
         this.authenticationManager = authenticationManager;
-        this.authenticationService = authenticationService;
         this.adminService = adminService;
     }
 
+    @Operation(summary = "Permite o login de usuários administradores já cadastrados")
     @PostMapping("/auth/login")
     public ResponseEntity<Object> login (@RequestBody @Valid AuthenticationDTO data){
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
@@ -45,27 +44,31 @@ public class AdminController {
         return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
-    @PostMapping("/auth/register")
-    public ResponseEntity<String> register(@RequestBody @Valid RegisterDTO registerDTO){
-        authenticationService.register(registerDTO, UserRole.ADMIN);
-        return ResponseEntity.ok().body("User registered successfully");
-    }
-
+    @Operation(summary = "Obtém todos os usuários com permissões básicas")
     @GetMapping("/users")
     public ResponseEntity<List<UserResponseDTO>> getAllUsers(){
         List<UserResponseDTO> userList = adminService.getAllUsers();
         return ResponseEntity.ok().body(userList);
     }
 
+    @Operation(summary = "Obtém todos os usuários com permissões de administrador")
     @GetMapping("/admins")
     public ResponseEntity<List<UserResponseDTO>> getAllAdmins(){
         List<UserResponseDTO> adminList = adminService.getAllAdmins();
         return ResponseEntity.ok().body(adminList);
     }
 
+    @Operation(summary = "Obtém um usuário pelo seu id (independe da permissão)")
     @GetMapping("/user/{id}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable String id){
         UserResponseDTO user = adminService.getUserById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @Operation(summary = "Permite excluir um usuário comum pelo id")
+    @DeleteMapping("/user/{userId}")
+    public ResponseEntity<String> deleteAdminById(@PathVariable("userId") String userId){
+        adminService.deleteUserById(userId);
+        return ResponseEntity.ok().body("User deleted successfully");
     }
 }
