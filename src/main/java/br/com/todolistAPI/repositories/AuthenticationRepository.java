f3e30c946da0b79e10e911d094c3ebd6e1a9133a
Davi Pedro <davisilvaps02@gmail.com>
2024-01-27 19:15:42 -0300

    findByRole

--- src/main/java/br/com/todolistAPI/repositories/AuthenticationRepository.java
+++ src/main/java/br/com/todolistAPI/repositories/AuthenticationRepository.java
@@ -1,9 +1,14 @@
 package br.com.todolistAPI.repositories;
 
 import br.com.todolistAPI.domain.user.User;
+import br.com.todolistAPI.domain.user.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AuthenticationRepository extends JpaRepository<User,Long> {
     UserDetails findByUsername(String login);
+    Optional<List<User>> findByRole(UserRole role);
 }
