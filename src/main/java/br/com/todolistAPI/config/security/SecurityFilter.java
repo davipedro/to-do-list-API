c45395818e6bc7241407e7db2232adaaf3ff817b
Davi Pedro <davisilvaps02@gmail.com>
2024-01-27 19:23:42 -0300

    Imports optimization

--- src/main/java/br/com/todolistAPI/config/security/SecurityFilter.java
+++ src/main/java/br/com/todolistAPI/config/security/SecurityFilter.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 /**
  * Classe que define filtros que podem ser utilizados no filterChain (cadeia de filtros)
