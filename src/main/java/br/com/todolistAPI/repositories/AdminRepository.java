27a256c2868dffbe03bc4b5008bea75897c70be2
Davi Pedro <davisilvaps02@gmail.com>
2024-02-25 15:42:53 -0300

    feat(pagination):pagination for AdminRepository

--- src/main/java/br/com/todolistAPI/repositories/AdminRepository.java
+++ src/main/java/br/com/todolistAPI/repositories/AdminRepository.java
@@ -2,11 +2,13 @@ package br.com.todolistAPI.repositories;
 
 import br.com.todolistAPI.domain.user.User;
 import br.com.todolistAPI.domain.user.UserRole;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<User, String> {
-    Optional<List<User>> findByRole(UserRole role);
+
+    Optional<Page<User>> findByRole(UserRole role, Pageable pageable);
 }
