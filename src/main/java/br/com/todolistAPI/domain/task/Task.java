bd3f6d2094ad3cca11bc420220170690d8bed1c1
Davi Pedro <davisilvaps02@gmail.com>
2024-02-25 15:55:14 -0300

    chore(imports): changing for explicit jakarta persistence imports

--- src/main/java/br/com/todolistAPI/domain/task/Task.java
+++ src/main/java/br/com/todolistAPI/domain/task/Task.java
@@ -2,7 +2,14 @@ package br.com.todolistAPI.domain.task;
 
 import br.com.todolistAPI.DTOs.TaskCreateDTO;
 import br.com.todolistAPI.domain.user.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import java.time.LocalDate;
 import java.util.UUID;
