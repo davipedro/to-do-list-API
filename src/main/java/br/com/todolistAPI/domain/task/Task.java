f07569f941e1d2ff1d5788a4b34ef08c8d5adaf5
Davi Pedro <davisilvaps02@gmail.com>
2024-02-27 14:35:27 -0300

    feat(Task): adding Priority attribute and Tag table relation

--- src/main/java/br/com/todolistAPI/domain/task/Task.java
+++ src/main/java/br/com/todolistAPI/domain/task/Task.java
@@ -1,17 +1,21 @@
 package br.com.todolistAPI.domain.task;
 
-import br.com.todolistAPI.DTOs.TaskCreateDTO;
 import br.com.todolistAPI.domain.user.User;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Entity(name = "task")
@@ -28,21 +32,30 @@ public class Task {
     private LocalDate creationDate;
     @Column(name = "conclusion_date")
     private LocalDate conclusionDate;
+    @Column(name = "last_update")
     private LocalDate lastUpdate;
     private Boolean completed = Boolean.FALSE;
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @OneToMany(mappedBy = "task", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Tag> tags;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    public Task(TaskCreateDTO taskCreateDTO){
-        this.title = taskCreateDTO.title();
-        this.description = taskCreateDTO.description();
+    public Task(String title, String description, LocalDate conclusionDate, List<Tag> tags, Priority priority){
+        this.title = title;
+        this.description = description;
         this.creationDate = LocalDate.now();
-        this.conclusionDate = taskCreateDTO.conclusionDate();
+        this.conclusionDate = conclusionDate;
         this.lastUpdate = LocalDate.now();
+        this.tags = tags;
+        this.priority = priority;
     }
 
-    public Task(String title, String description,LocalDate currentDate ,LocalDate conclusionDate){
+    public Task(String title, String description, LocalDate currentDate ,LocalDate conclusionDate){
         this.title = title;
         this.description = description;
         this.creationDate = currentDate;
@@ -106,5 +119,25 @@ public class Task {
 
     public void setCompleted(Boolean completed) {
         this.completed = completed;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public User getUser() {
+        return user;
     }
 }
