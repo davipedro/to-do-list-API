89aff053d2ef701f873b472fd10e6ace3bf69340
Davi Pedro <davisilvaps02@gmail.com>
2024-02-27 14:40:57 -0300

    feat(TaskDTO): adding Tags and Priority fields on TaskCreateDTO

--- src/main/java/br/com/todolistAPI/DTOs/TaskCreateDTO.java
+++ src/main/java/br/com/todolistAPI/DTOs/TaskCreateDTO.java
@@ -1,13 +1,18 @@
 package br.com.todolistAPI.DTOs;
 
+import br.com.todolistAPI.domain.task.Tag;
+import br.com.todolistAPI.domain.task.Priority;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record TaskCreateDTO (@JsonAlias("title") @NotBlank String title,
-                            @JsonAlias("description") String description,
-                            @JsonAlias("conclusion_date") LocalDate conclusionDate){
+                             @JsonAlias("description") String description,
+                             @JsonAlias("conclusion_date") LocalDate conclusionDate,
+                             @JsonAlias("custom_tags") List<Tag> tags,
+                             @JsonAlias("priority") Priority priority){
 }
