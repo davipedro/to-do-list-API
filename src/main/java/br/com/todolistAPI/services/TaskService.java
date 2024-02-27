40796eb531e00121cec1e4b02c3850f04d80dfc9
Davi Pedro <davisilvaps02@gmail.com>
2024-02-27 14:54:36 -0300

    feat(Task): adding addTagToTask, getByTagName and returning a DTO in putTask in TaskService

--- src/main/java/br/com/todolistAPI/services/TaskService.java
+++ src/main/java/br/com/todolistAPI/services/TaskService.java
@@ -1,7 +1,10 @@
 package br.com.todolistAPI.services;
 
 import br.com.todolistAPI.DTOs.TaskCreateDTO;
+import br.com.todolistAPI.DTOs.TaskResponseDTO;
 import br.com.todolistAPI.DTOs.TaskUpdateDTO;
+import br.com.todolistAPI.domain.task.Priority;
+import br.com.todolistAPI.domain.task.Tag;
 import br.com.todolistAPI.domain.task.Task;
 import br.com.todolistAPI.exceptions.task.TaskCouldNotBeCreated;
 import br.com.todolistAPI.exceptions.task.TaskNotFoundException;
@@ -9,6 +12,7 @@ import br.com.todolistAPI.repositories.TaskRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,7 +25,7 @@ public class TaskService {
     }
 
     public void createTask(TaskCreateDTO taskToSave){
-        Task task = new Task(taskToSave);
+        Task task = new Task(taskToSave.title(), taskToSave.description(), taskToSave.conclusionDate(), taskToSave.tags(), taskToSave.priority());
         try {
             taskRepository.save(task);
         } catch (RuntimeException e){
@@ -41,21 +45,63 @@ public class TaskService {
     public List<Task> getByCreationDate(LocalDate creationDate){
         return taskRepository.findByCreationDate(creationDate)
                 .orElseThrow(() -> new TaskNotFoundException("Task(s) do not found"));
-
     }
 
-    public void putTask(UUID taskId, TaskUpdateDTO taskUpdateDTO) {
+    public List<Task> getByTagName(String tagName){
+        return taskRepository.findByTagName(tagName).orElse(Collections.emptyList());
+    }
+
+    public TaskResponseDTO putTask(UUID taskId, TaskUpdateDTO taskUpdateDTO) {
         Task task =  taskRepository.findById(taskId)
                 .orElseThrow(() -> new TaskNotFoundException("Task do not found"));
 
         if (!(taskUpdateDTO.title().isEmpty())) task.setTitle(taskUpdateDTO.title());
+
         if (!(taskUpdateDTO.description().isEmpty())) task.setDescription(taskUpdateDTO.description());
+
         if (!taskUpdateDTO.completed().equals(task.getCompleted())) task.setCompleted(taskUpdateDTO.completed());
+
         if (taskUpdateDTO.conclusionDate() != null) task.setConclusionDate(taskUpdateDTO.conclusionDate());
+
+        if (!taskUpdateDTO.tags().isEmpty()) addTagToTask(task, taskUpdateDTO);
+
+        if (taskUpdateDTO.priority() != null && Priority.isPriority(taskUpdateDTO.priority())){
+            task.setPriority(taskUpdateDTO.priority());
+        }
         task.setLastUpdate(LocalDate.now());
 
         taskRepository.save(task);
+
+        return new TaskResponseDTO(
+                task.getTitle(),
+                task.getDescription(),
+                task.getCreationDate(),
+                task.getConclusionDate(),
+                task.getLastUpdate(),
+                task.getCompleted(),
+                task.getPriority()
+        );
     }
+
+    private void addTagToTask(Task task, TaskUpdateDTO taskUpdateDTO){
+
+        List<Tag> currentTags = task.getTags();
+
+        List<Tag> newTags = taskUpdateDTO.tags()
+                .stream().map(tagUpdateDTO -> new Tag(
+                        task.getUser(),
+                        tagUpdateDTO.tagName(),
+                        tagUpdateDTO.tagColor())
+                ).toList();
+
+        for (Tag newTag : newTags) {
+            if (!currentTags.contains(newTag)) {
+                currentTags.add(newTag);
+            }
+        }
+        task.setTags(currentTags);
+    }
+
     public void deleteTask(UUID taskId){
         Task task = taskRepository.findById(taskId)
                 .orElseThrow(() -> new TaskNotFoundException("Task do not found"));
