0b590d825cb6dec808619baa06c0d36ac8fc342f
Davi Pedro <davisilvaps02@gmail.com>
2024-02-27 14:56:31 -0300

    feat(Task): adding Tags and Priority fields to TaskUpdateDTO

--- src/main/java/br/com/todolistAPI/DTOs/TaskUpdateDTO.java
+++ src/main/java/br/com/todolistAPI/DTOs/TaskUpdateDTO.java
@@ -1,13 +1,17 @@
 package br.com.todolistAPI.DTOs;
 
+import br.com.todolistAPI.domain.task.Priority;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record TaskUpdateDTO (@JsonAlias("title") String title,
                              @JsonAlias("description") String description,
                              @JsonAlias("conclusion_date") LocalDate conclusionDate,
-                             @JsonAlias("completed") Boolean completed){
+                             @JsonAlias("completed") Boolean completed,
+                             @JsonAlias("custom_tags") List<TagUpdateDTO> tags,
+                             @JsonAlias("priority") Priority priority){
 }
