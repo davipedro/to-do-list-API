5b27ee23fb851958556ef2eb073bde6050e25722
Davi Pedro <davisilvaps02@gmail.com>
2024-05-07 17:30:40 -0300

    Small adjustments

--- src/main/java/br/com/todolistAPI/repositories/TaskRepository.java
+++ src/main/java/br/com/todolistAPI/repositories/TaskRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, UUID> {
         Optional<List<Task>> findByTitleContainingIgnoreCase(String title);
         Optional<List<Task>> findByCreationDate(LocalDate creationDate);
         Optional<Task> findById(UUID taskId);
